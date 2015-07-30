1d193276693187ed44995dd6b7fd865e4f6ad75c
Benjamin Klix <benjamin.klix@gmail.com>
2015-07-30 08:54:55 +0200

    [TASK] Small changes to "replaceSmiley" function

--- src/helper.js
+++ src/helper.js
@@ -23,7 +23,7 @@
       xhr.send();
     },
 
-    replaceSmiley: function(node, smiley) {
+    replaceSmiley: function(node, smiley, start) {
       var icon = config.icons[smiley],
           img = document.createElement('img');
       img.className = icon['class'];
@@ -31,8 +31,8 @@
       img.alt = icon.alt;
       img.setAttribute('draggable', config.additionalAttributes.draggable);
       helpers.range = helpers.selection.getRangeAt(0);
-      helpers.range.setStart(node, helpers.selection.anchorOffset - smiley.length);
-      helpers.range.setEnd(node, helpers.selection.anchorOffset);
+      helpers.range.setStart(node, start);
+      helpers.range.setEnd(node, start + smiley.length);
       helpers.range.deleteContents();
       helpers.range.insertNode(img);
       helpers.range.setStartAfter(img);
@@ -52,7 +52,7 @@
       for (var string in config.icons) {
         if (helpers.selection.anchorOffset - string.length > -1 &&
             message.substr(helpers.selection.anchorOffset - string.length, helpers.selection.anchorOffset) === string) {
-          helpers.replaceSmiley(helpers.selection.anchorNode, string);
+          helpers.replaceSmiley(helpers.selection.anchorNode, string, helpers.selection.anchorOffset - string.length);
           break;
         }
       }
