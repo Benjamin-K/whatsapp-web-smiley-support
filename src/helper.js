ec7e0ca7f6d15b42b667f8981eb83107186305c7
Benjamin Klix <benjamin.klix@gmail.com>
2015-07-30 08:48:33 +0200

    [UPDATE] Change the way smileys are replaced

--- src/helper.js
+++ src/helper.js
@@ -5,7 +5,10 @@
 
   var config = {
     icons: {},
-    additionalAttributes: 'draggable="false" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"'
+    additionalAttributes: {
+      draggable: false,
+      src: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+    }
   };
 
   var helpers = {
@@ -19,35 +22,37 @@
       xhr.open("GET", chrome.extension.getURL('/config/'+config+'.json'), true);
       xhr.send();
     },
-    focusEnd: function(element) {
-      element.focus();
 
-      var node = document.createElement('span');
-
-      element.appendChild(node);
-
-      helpers.range = document.createRange();
-      helpers.range.selectNode(node);
+    replaceSmiley: function(node, smiley) {
+      var icon = config.icons[smiley],
+          img = document.createElement('img');
+      img.className = icon['class'];
+      img.src = config.additionalAttributes.src;
+      img.alt = icon.alt;
+      img.setAttribute('draggable', config.additionalAttributes.draggable);
+      helpers.range = helpers.selection.getRangeAt(0);
+      helpers.range.setStart(node, helpers.selection.anchorOffset - smiley.length);
+      helpers.range.setEnd(node, helpers.selection.anchorOffset);
+      helpers.range.deleteContents();
+      helpers.range.insertNode(img);
+      helpers.range.setStartAfter(img);
+      helpers.range.setEndAfter(img);
       helpers.selection.removeAllRanges();
-      helpers.selection.addRange(range);
-
-      element.removeChild(node);
+      helpers.selection.addRange(helpers.range);
     },
+
     selection: window.getSelection(),
     range: null
   };
 
   document.addEventListener('keyup', function(e) {
-    if (e.target.isContentEditable) {
-      var message = e.target.innerHTML;
+    if (e.target.isContentEditable && helpers.selection.anchorNode.nodeType === 3) {
+      var message = helpers.selection.anchorNode.nodeValue;
 
-      for (var string in icons) {
-        if (message.length - string.length > -1 &&
-            message.lastIndexOf(string) === message.length - string.length) {
-          e.target.innerHTML = message.substr(0, message.length - string.length) +
-            '<img alt="'+icons[string].alt+'" class="'+icons[string]['class']+'" '+
-                  config.additionalAttributes+'>';
-          helpers.focusEnd(e.target);
+      for (var string in config.icons) {
+        if (helpers.selection.anchorOffset - string.length > -1 &&
+            message.substr(helpers.selection.anchorOffset - string.length, helpers.selection.anchorOffset) === string) {
+          helpers.replaceSmiley(helpers.selection.anchorNode, string);
           break;
         }
       }
